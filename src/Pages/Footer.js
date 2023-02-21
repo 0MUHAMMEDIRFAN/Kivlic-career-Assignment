6aae289f446ee8eb6888c76314964bdb660ffd39
muhammedirfan <irfannadapuram>
2023-02-21 07:29:32 +0530

    footer completed

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <div className='footer'>
             <div className="about">
                 <img src={logoFooter} alt="" />
-                <p>KIVLIK is a mobile steam car wash company, which offers premium car wash and detailing services at your doorstep. Conveniently book a car wash with a few taps on your phone. KIVLIK has several packages to meet your budget and requirements. Conveniently book a carwash with a few taps using the KIVLIK App or website. You can pick a time, location and package from the App or web. KIVLIK washes each vehicle using our Steam Cleaning system.
+                <p>KIVLIK is a mobile steam car wash company, which offers premium car wash and detailing services at your doorstep. Conveniently book a car wash with a few taps on your phone. <br /> KIVLIK has several packages to meet your budget and requirements. Conveniently book a carwash with a few taps using the KIVLIK App or website. You can pick a time, location and package from the App or web. KIVLIK washes each vehicle using our Steam Cleaning system.
                 </p>
             </div>
             <div className="company">
@@ -21,14 +21,16 @@ function Footer() {
                 <p> Investor Relations</p>
             </div>
             <div className="question">
-                <p>Got Questions</p>
-                <h4>Help Center</h4>
-                <h6>Follow Us</h6>
+                <p>Got Questions?</p>
+                <h3>Help Center</h3>
                 <div className="social">
-                <i className='bx bxl-facebook'  ></i>
-                <i className='bx bxl-instagram' ></i>
-                <i className='bx bxl-twitter' ></i>
-                <i className='bx bxl-youtube' ></i>
+                    <h3 className='followUs'>Follow Us</h3>
+                    <div className="icons">
+                        <i className='bx bxl-facebook'  ></i>
+                        <i className='bx bxl-instagram' ></i>
+                        <i className='bx bxl-twitter' ></i>
+                        <i className='bx bxl-youtube' ></i>
+                    </div>
                 </div>
                 <div className="store">
                     <img src={appstore} alt="" />
